7c0638f10a9eecfaf267719e68e8c0a934ca6695
Eduardo San Martin Morote <posva13@gmail.com>
2019-07-21 18:17:37 +0200

    Fix typings for GameConfig scene

--- src/scene/typedefs/ScenePreloadCallback.js
+++ src/scene/typedefs/ScenePreloadCallback.js
@@ -3,6 +3,7 @@
  * This method is called by the Scene Manager, after `init()` and before `create()`, only if the Scene has a LoaderPlugin.
  * After this method completes, if the LoaderPlugin's queue isn't empty, the LoaderPlugin will start automatically.
  *
+ * @this {Phaser.Scene}
  * @callback Phaser.Types.Scenes.ScenePreloadCallback
  * @since 3.0.0
  */
